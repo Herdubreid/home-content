8a7869f0ad0278c2fc9fa76f2b3ad2a9d339840c
Finnur Bragason <fbragason@outlook.com>
2020-01-23 12:58:27 +0800

    .

---
+++ Services/Content.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Home.Content
+{
+    public class NugetVersion
+    {
+        public string version { get; set; }
+        public int downloads { get; set; }
+        public string @id { get; set; }
+    }
+    public class NugetData
+    {
+
+        public string @id { get; set; }
+        public string @type { get; set; }
+        public string registration { get; set; }
+        public string version { get; set; }
+        public string description { get; set; }
+        public string summary { get; set; }
+        public string title { get; set; }
+        public string iconUrl { get; set; }
+        public string licenseUrl { get; set; }
+        public string projectUrl { get; set; }
+        public string[] tags { get; set; }
+        public string[] authors { get; set; }
+        public int totalDownloads { get; set; }
+        public bool verified { get; set; }
+        public NugetVersion[] versions { get; set; }
+    }
+    public class NugetResponse
+    {
+        public int totalHits { get; set; }
+        public NugetData[] data { get; set; }
+    }
+    public class Nuget
+    {
+        public string PackageId { get; set; }
+        public string Comments { get; set; }
+        public NugetData Data { get; set; }
+    }
+    public class BlogPost
+    {
+        public string Href { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Intro { get; set; }
+    }
+    public class VideoPost
+    {
+        public string Post { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<string> Body { get; set; }
+    }
+}
---
+++ Services/ContentService.cs
@@ -0,0 +1,114 @@
+﻿using HtmlKit;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    public class ContentService
+    {
+        HttpClient Http { get; }
+        public async Task<Content.VideoPost[]> GetLinkedinPostsAsync()
+        {
+            return await Http.GetJsonAsync<Content.VideoPost[]>("linkedin.json");
+        }
+        public async Task<Content.VideoPost[]> GetMsdnPostsAsync()
+        {
+            return await Http.GetJsonAsync<Content.VideoPost[]>("msdn.json");
+        }
+        public async Task<Content.VideoPost[]> GetYoutubeClipsAsync()
+        {
+            return await Http.GetJsonAsync<Content.VideoPost[]>("youtube.json");
+        }
+        public async Task<Content.Nuget[]> GetNugetAsync()
+        {
+            var nuget = await Http.GetJsonAsync<Content.Nuget[]>("nuget.json");
+            foreach (var n in nuget)
+            {
+                var rs = await Http.GetAsync($"https://azuresearch-usnc.nuget.org/query?q=PackageId:{n.PackageId}");
+                var ngRs = JsonSerializer.Deserialize<Content.NugetResponse>(await rs.Content.ReadAsStringAsync());
+                if (ngRs.totalHits > 0)
+                {
+                    n.Data = ngRs.data[0];
+                }
+            }
+            return nuget;
+        }
+        public async Task<List<Content.BlogPost>> GetBlogPostsAsync()
+        {
+            var rs = await Http.GetAsync("https://blog.celin.io");
+            var reader = new StreamReader(rs.Content.ReadAsStreamAsync().Result);
+            var tokenizer = new HtmlTokenizer(reader);
+            bool start = false;
+            bool blogs = false;
+            List<Content.BlogPost> blog = new List<Content.BlogPost>();
+            while (tokenizer.ReadNextToken(out var token))
+            {
+                var tag = token as HtmlTagToken;
+                if (start)
+                {
+                    if (token.Kind == HtmlTokenKind.Tag && "a".Equals(tag.Name))
+                    {
+                        blogs = true;
+                        var href = tag.Attributes.FirstOrDefault(a => "href".Equals(a.Name));
+                        var post = new Content.BlogPost()
+                        {
+                            Href = href.Value
+                        };
+                        bool endTag = false;
+                        while (tokenizer.ReadNextToken(out var postToken) && !endTag)
+                        {
+                            if (postToken.Kind == HtmlTokenKind.Tag)
+                            {
+                                var postTag = postToken as HtmlTagToken;
+                                if (postTag.IsEndTag)
+                                {
+                                    endTag = "a".Equals(postTag.Name);
+                                }
+                                else
+                                {
+                                    if ("time".Equals(postTag.Name))
+                                    {
+                                        var dt = postTag.Attributes.FirstOrDefault(a => "datetime".Equals(a.Name));
+                                        post.Date = DateTime.Parse(dt.Value);
+                                    }
+                                    if ("h1".Equals(postTag.Name)
+                                        && tokenizer.ReadNextToken(out var pToken))
+                                    {
+                                        var content = pToken as HtmlDataToken;
+                                        post.Title = content.Data;
+                                    }
+                                    if ("p".Equals(postTag.Name)
+                                        && tokenizer.ReadNextToken(out var hToken))
+                                    {
+                                        var content = hToken as HtmlDataToken;
+                                        post.Intro = content.Data;
+                                    }
+                                }
+                            }
+                        }
+                        blog.Add(post);
+                    }
+                    else if (blogs)
+                    {
+                        start = false;
+                    }
+                }
+                else
+                {
+                    start = token.Kind == HtmlTokenKind.Tag && "main".Equals(tag.Name);
+                }
+            }
+            return blog;
+        }
+        public ContentService(HttpClient http)
+        {
+            Http = http;
+        }
+    }
+}
